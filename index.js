4a670abc8033f8743f86e8aabac60880b0aac551
jsanthos01 <chinu.s2001@gmail.com>
2020-03-01 19:01:42 -0500

    changes to contact.html

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready( function() {
+  
+
+    //Page navigation
+    $("header").on("click", function() {
+        $('html, body').animate({
+            scrollTop: $("r").offset().top
+        }, 800);
+    });
+    //Move title to header after 1st viewport
+    $(document).scroll(function() {
+        var y = $(this).scrollTop();
+        if (y > 500) {
+          $("#header").fadeIn();
+        } else {
+          $("#header").fadeOut();
+        }
+      });
+
+});
