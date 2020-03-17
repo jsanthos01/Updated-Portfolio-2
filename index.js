5f033efec5b2f36a4786c5ef78ebd9478ac06dd2
jsanthos01 <chinu.s2001@gmail.com>
2020-03-17 15:14:02 -0400

    added animations using aos

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-$(document).ready( function() {
-  
-
+$(document).ready( async function() {
+    
     //Page navigation
     $("header").on("click", function() {
-        $('html, body').animate({
-            scrollTop: $("r").offset().top
-        }, 800);
+      $('html, body').animate({
+          scrollTop: $("r").offset().top
+      }, 800);
+  });
+  //Move title to header after 1st viewport
+  $(document).scroll(function() {
+      var y = $(this).scrollTop();
+      if (y > 500) {
+        $("#header").fadeIn();
+      } else {
+        $("#header").fadeOut();
+      }
     });
-    //Move title to header after 1st viewport
-    $(document).scroll(function() {
-        var y = $(this).scrollTop();
-        if (y > 500) {
-          $("#header").fadeIn();
-        } else {
-          $("#header").fadeOut();
-        }
-      });
 
 });
